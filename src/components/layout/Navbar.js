0cfd62c3913bfb328593098cb383b6f96e983b99
Hemant Saini <hsainioff@gmail.com>
2020-07-26 13:33:38 +0530

    Navbar Updated

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Navbar(props) {
   return (
     <nav className='navbar bg-primary '>
@@ -7,6 +7,14 @@ function Navbar(props) {
         <i className='fa fa-github'></i>
         {props.title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
